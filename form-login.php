846a853c992e0b9e49751b27870c552b31ac4767
LKubner <luciano.2022310952@aluno.iffar.edu.br>
2024-07-04 11:40:27 -0300

    fiz login, cadastrar e o conexao

---
+++ form-login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form action="login.php" method="post">
+        <label> Email: <input type="email" name="email"> <br></lable>
+            <label> Senha: <input type="password" name="senha"> </label> <br>
+            <input type="submit" value="login"> <br>
+    </form> <br> <br>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,13 +1,35 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Legacy Arena</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <title>Login</title>
+
+    <style>
+    p {
+      position:absolute;
+      top: -30px;
+      font-size: 25px;
+    }
+
+    </style>
+   
 </head>
+
 <body>
-  <h1> <center> Bem vindo!! </center> </h1>
-   <a href="cadastro.php"> Cadastre-se </a> <br>
-   <a href="login.php"> Login </a>  
+<nav>
+    <div class="nav-wrapper">
+    <p> Legacy Arena </p>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="form-cadastro.php">Cadastrar-se</a></li>
+        <li><a href="form-login.php">Login</a></li>
+        <a href="form-recuperar-senha.php"> Recuperar senha </a> <br>
+      </ul>
+    </div>
+  </nav>
+
+  
 </body>
 </html>
