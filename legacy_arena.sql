bbb9c76e8c78d52a30ea33c4cd7c6f8ba5b787da
LKubner <luciano.2022310952@aluno.iffar.edu.br>
2024-08-08 15:26:02 -0300

    mysql do banco

--- legacy_arena.sql
+++ legacy_arena.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Tempo de geração: 01-Ago-2024 às 20:18
+-- Tempo de geração: 08-Ago-2024 às 18:25
 -- Versão do servidor: 8.0.31
 -- versão do PHP: 8.0.26
 
@@ -18,8 +18,21 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Banco de dados: `legacyarena`
+-- Banco de dados: `legacy_arena`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `equipe`
+--
+
+DROP TABLE IF EXISTS `equipe`;
+CREATE TABLE IF NOT EXISTS `equipe` (
+  `id_equipe` int NOT NULL,
+  `nome` int NOT NULL,
+  PRIMARY KEY (`id_equipe`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -29,7 +42,8 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `rankingcs`;
 CREATE TABLE IF NOT EXISTS `rankingcs` (
-  `id_equipes` int NOT NULL AUTO_INCREMENT,
+  `grupo` char(7) NOT NULL,
+  `id_equipe` int NOT NULL,
   `partidas` int NOT NULL,
   `pontos` int NOT NULL,
   `vitoria` int NOT NULL,
@@ -39,9 +53,19 @@ CREATE TABLE IF NOT EXISTS `rankingcs` (
   `rounds_perdidos` int NOT NULL,
   `confronto_direito` int NOT NULL,
   `wo` varchar(255) NOT NULL,
-  `grupo` char(7) NOT NULL,
-  PRIMARY KEY (`id_equipes`)
+  PRIMARY KEY (`grupo`),
+  KEY `fk_id_equipe` (`id_equipe`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `rankingcs`
+--
+ALTER TABLE `rankingcs`
+  ADD CONSTRAINT `fk_id_equipe` FOREIGN KEY (`id_equipe`) REFERENCES `equipe` (`id_equipe`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
