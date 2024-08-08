e9220affbe67474ee218c730f8a5fdb8f9d4de24
LKubner <luciano.2022310952@aluno.iffar.edu.br>
2024-08-08 15:25:11 -0300

    começando o crud
    asasdasd

---
+++ admchaveamentocs.php
@@ -0,0 +1,48 @@
+<?php
+    require_once "conexao.php";
+
+
+
+
+    ?>
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Grupos CS</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <a href="grupocs.php">Novo Grupo</a>
+    <table border="1">
+        <tr>
+            <th>Grupo A</th>
+            <th>Partidas</th>
+            <th>Vitórias</th>
+            <th>Derrotas</th>
+            <th>Dif. Round</th>
+            <th>Pontos</th>
+            <th>Ações</th>
+        </tr>
+        <?php foreach ($grupos as $grupo): ?>
+            <tr>
+                <td><?php echo ($grupo['nome_grupo']); ?></td>
+                <td><?php echo ($grupo['nome_equipe']); ?></td>
+                <td><?php echo ($grupo['vitorias']); ?></td>
+                <td><?php echo ($grupo['derrotas']); ?></td>
+                <td><?php echo ($grupo['empates']); ?></td>
+                <td><?php echo ($grupo['pontos']); ?></td>
+                <td>
+                    <a href="updategrupocs.php?id_grupo=<?php echo ($grupo['id_grupo']); ?>">Editar</a>
+                    <a href="deletegrupocs.php?id_grupo=<?php echo ($grupo['id_grupo']); ?>">Deletar</a>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+    </table>
+</body>
+</html>
--- chaveamentocs.php
+++ chaveamentocs.php
@@ -3,11 +3,6 @@ require_once "conexao.php";
 
 
 
-
-
-
-
-
 ?>
 
 
@@ -48,10 +43,6 @@ require_once "conexao.php";
     <td> 6 </td>
   </tr>
 </table> 
-<div class="row">
-        <div class="col s1" value="1">Visão Geral</div>
-        <div class="col s1" value="2"> <a href="chaveamento.php"> Chaveamento </a> </div>
-        <div class="col s1" value="3">Ranking</div>
-        <div class="col s1" value="4"> <a href="rankingteste.php"> Outro</div>
 </body>
 </html>
+
