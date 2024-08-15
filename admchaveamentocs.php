d8e9b928bb26587d199908e25ae261649c8c0052
LKubner <luciano.2022310952@aluno.iffar.edu.br>
2024-08-15 15:27:38 -0300

    agora o inserir funciona com varios grupos diff.
    criar um crud individual para a equipe (substituir o select name "grupo" por select name="equipe" aí o grupo será setado já automaticamente

--- admchaveamentocs.php
+++ admchaveamentocs.php
@@ -9,11 +9,16 @@ if ($resultado) {
     echo mysqli_errno($conexao) . ": " . mysqli_error($conexao);
 }
 
-$sql2 = "SELECT cs.*, eq.nome, eq.foto_time FROM rankingcs cs INNER JOIN equipe eq ON cs.id_equipe = eq.id_equipe";
+$sql2 = "SELECT cs.*, eq.nome, eq.foto_time FROM rankingcs cs INNER JOIN equipe eq ON cs.id_equipe = eq.id_equipe ORDER BY cs.grupo";
 $resultado1 = mysqli_query($conexao, $sql2);
 
-$grupo = mysqli_fetch_assoc($resultado1)['grupo'];
-mysqli_data_seek($resultado1, 0);
+$teste = mysqli_fetch_assoc($resultado1);
+if ($teste != null) {
+    $grupo = $teste['grupo'];
+    mysqli_data_seek($resultado1, 0);
+} else {
+    $grupo = '';
+}
 
 ?>
 
@@ -33,44 +38,52 @@ mysqli_data_seek($resultado1, 0);
 
 <body>
     <a href="crud/form-grupocs.php">Novo Grupo</a>
-    <table border="1">
-        <tr>
-        <th>Grupo <?php echo ($grupo); ?></th>
-            <th>Nome</th>
-            <th>Partidas</th>
-            <th>Vitórias</th>
-            <th>Derrotas</th>
-            <th>Dif. Round</th>
-            <th>Pontos</th>
-        </tr>
 
-        <tbody>
+    <?php
+    $grp = '';
+    $primeira = true;
+    while ($dados = mysqli_fetch_assoc($resultado1)) {
+        if ($grp != $dados['grupo']) {
+            $grp = $dados['grupo'];
 
-            <?php
-
-            while ($dados = mysqli_fetch_assoc($resultado1)) {
-                print_r($dados); 
-                $foto_time = 'imagens/' . (isset($dados['foto_time']) ? $dados['foto_time'] : 'default.png');
-                // Verifique se a imagem existe
-                if (!file_exists($foto_time)) {
-                    $foto_time = 'imagens/default.png'; // Caminho para uma imagem padrão se a imagem não for encontrada
-                }
-                echo "<tr>";
-                echo "<td> <img src='" . $foto_time . "' alt='foto do time' style='width:32px;height:28px;'></td>";
-                echo "<td>" . $dados['nome'] . "</td>";
-                echo "<td>" . $dados['partidas'] . "</td>";
-                echo "<td>" . $dados['vitoria'] . "</td>";
-                echo "<td>" . $dados['derrota'] . "</td>";
-                echo "<td>" . $dados['dif_round'] . "</td>";
-                echo "<td>" . $dados['pontos'] . "</td>";
-                echo "</tr>";
+            if (!$primeira) {
+                echo "</tbody></table><br>";
             }
+            $primeira = false;
+    ?>
+            <table border="1">
+                <thead>
+                    <tr>
+                        <th>Grupo <?= $dados['grupo']; ?></th>
+                        <th>Nome</th>
+                        <th>Partidas</th>
+                        <th>Vitórias</th>
+                        <th>Derrotas</th>
+                        <th>Dif. Round</th>
+                        <th>Pontos</th>
+                    </tr>
+                </thead>
+                <tbody>
+            <?php
+        }
 
-            ?>
-
-        </tbody>
-
-    </table>
+        //print_r($dados); 
+        $foto_time = 'imagens/' . (isset($dados['foto_time']) ? $dados['foto_time'] : 'default.png');
+        // Verifique se a imagem existe
+        if (!file_exists($foto_time)) {
+            $foto_time = 'imagens/default.png'; // Caminho para uma imagem padrão se a imagem não for encontrada
+        }
+        echo "<tr>";
+        //echo "<td> <img src='" . $foto_time . "' alt='foto do time' style='width:32px;height:28px;'></td>";
+        echo "<td></td>";
+        echo "<td>" . $dados['nome'] . "</td>";
+        echo "<td>" . $dados['partidas'] . "</td>";
+        echo "<td>" . $dados['vitoria'] . "</td>";
+        echo "<td>" . $dados['derrota'] . "</td>";
+        echo "<td>" . $dados['dif_round'] . "</td>";
+        echo "<td>" . $dados['pontos'] . "</td>";
+        echo "</tr>";
+    } ?>
 </body>
 
 </html>
