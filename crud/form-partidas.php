405dec8fdd368281355e8cf67534b8968fd81e86
LKubner <luciano.2022310952@aluno.iffar.edu.br>
2024-10-24 16:56:18 -0300

    arrumando css e adicionando o select de partidas

--- crud/form-partidas.php
+++ crud/form-partidas.php
@@ -66,3 +66,9 @@ SELECT p.id_partida,
        (SELECT e.foto_time FROM equipe e WHERE e.id_equipe = p.id_equipe2) AS foto_time2, 
        p.resultado2 
 FROM partidas p;
+
+
+
+resolvido abaixo
+
+SELECT p.id_partida, (SELECT e.nome FROM equipe e WHERE e.id_equipe = p.id_equipe) AS nome_equipe1, (SELECT e.foto_time FROM equipe e WHERE e.id_equipe = p.id_equipe) AS foto_time1, p.resultado, (SELECT e.nome FROM equipe e WHERE e.id_equipe = p.id_equipe2) AS nome_equipe2, (SELECT e.foto_time FROM equipe e WHERE e.id_equipe = p.id_equipe2) AS foto_time2, p.resultado2 FROM partidas p;
---
+++ freefire.php
@@ -0,0 +1,42 @@
+<?php
+include_once "conexao.php";
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/style.css">
+  <script src="../js.js"></script>
+  <title>Legacy</title>
+</head>
+<body class= "bodyfree">
+  <?php
+  // Navbar e Sidebar 
+  include_once "header.php";
+  ?>
+ <div id="main-content">
+    <h1 class="titulo"> Free Fire</h1>
+    <style>
+        .col { cursor: pointer; }
+    </style>
+</head>
+<body>
+<div id="container">
+<div class="centralizar-link">
+    <a href="chaveamentocs.php">Chaveamento</a>
+</div>
+<div class="centralizar-link"> <br>
+    <a href="partidacs.php">Partidas</a>
+</div>
+   
+    
+
+</div>
+
+  
+</div>
+</body>
+
+</html>
