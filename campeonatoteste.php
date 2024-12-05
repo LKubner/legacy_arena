44f23655b2b39ef2a1c4883c160626c8c1937a2a
LKubner <luciano.2022310952@aluno.iffar.edu.br>
2024-12-05 16:47:12 -0300

    mais ajustes

--- campeonatoteste.php
+++ campeonatoteste.php
@@ -5,68 +5,102 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="css/style.css">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
   <script src="../js.js"></script>
   <title>Legacy</title>
 </head>
 
-  <?php
-  // Navbar e Sidebar 
-  include_once "header.php";
-  $ano = $_GET['ano'];
-  echo "JEIF do ano $ano";
-  ?>
- <div id="main-content">
-    <h1 class="titulo">Bem-vindo ao Legacy Arena</h1>
+<?php
+// Navbar e Sidebar 
+include_once "header.php";
+$ano = $_GET['ano'];
+echo "JEIF do ano $ano";
+?>
+<div id="main-content">
+  <h1 class="titulo">Bem-vindo ao Legacy Arena</h1>
+
+  <div class="card custom-card">
+    <div class="card-image waves-effect waves-block waves-light">
+      <img class="activator" src="imagens/cs2.png" alt="Imagem do Card">
+    </div>
+    <div class="card-content">
+      <span class="card-title activator grey-text text-darken-4">Counter-Strike 2<i class="material-icons right">more_vert</i></span>
+      <p><a href="chaveamentocs.php">Acessar Classificação</a></p>
+    </div>
 
     <div class="card custom-card">
-  <div class="card-image waves-effect waves-block waves-light">
-    <img class="activator" src="imagens/cs2.png" alt="Imagem do Card">
-  </div>
-  <div class="card-content">
-    <span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
-    <p><a href="#">This is a link</a></p>
-  </div>
-  <div class="card-reveal">
-    <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
-    <p>Here is some more information about this product that is only revealed once clicked on.</p>
-  </div>
-</div>
+    <div class="card-image waves-effect waves-block waves-light">
+      <img class="activator" src="imagens/lol.png" alt="Imagem do Card">
+    </div>
+    <div class="card-content">
+      <span class="card-title activator grey-text text-darken-4">League of Legends<i class="material-icons right">more_vert</i></span>
+      <p><a href="chaveamentocs.php">Acessar Classificação</a></p>
+    </div>
 
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
+    <div class="card custom-card">
+    <div class="card-image waves-effect waves-block waves-light">
+      <img class="activator" src="imagens/valorant.png" alt="Imagem do Card">
+    </div>
+    <div class="card-content">
+      <span class="card-title activator grey-text text-darken-4">Valorant<i class="material-icons right">more_vert</i></span>
+      <p><a href="chaveamentocs.php">Acessar Classificação</a></p>
+    </div>
+
+    <div class="card custom-card">
+    <div class="card-image waves-effect waves-block waves-light">
+      <img class="activator" src="imagens/free.png" alt="Imagem do Card">
+    </div>
+    <div class="card-content">
+      <span class="card-title activator grey-text text-darken-4">Free Fire<i class="material-icons right">more_vert</i></span>
+      <p><a href="chaveamentocs.php">Acessar Classificação</a></p>
+    </div>
+
+    <div class="card custom-card">
+    <div class="card-image waves-effect waves-block waves-light">
+      <img class="activator" src="imagens/xadrezar.png" alt="Imagem do Card">
+    </div>
+    <div class="card-content">
+      <span class="card-title activator grey-text text-darken-4">Xadrez Arena<i class="material-icons right">more_vert</i></span>
+      <p><a href="chaveamentocs.php">Acessar Classificação</a></p>
+    </div>
+    
+  </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 </div>
 
