2724438a66994404dc07a5eff32e37da6612fdcd
LKubner <luciano.2022310952@aluno.iffar.edu.br>
2024-12-20 09:38:03 -0300

    Arrumando os cards

--- campeonatoteste.php
+++ campeonatoteste.php
@@ -4,8 +4,9 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/style.css?nocache=<?php rand(); ?>">
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
   <script src="../js.js"></script>
   <title>Legacy</title>
 </head>
@@ -15,35 +16,40 @@
 include_once "header.php";
 //atribuir ao banco de dados
 include_once "conexao.php";
+session_start();
 ?>
-
+<style> 
+  .imgcard{
+    object-fit: cover;
+  }
+</style>
 <body>
   <div id="main-content">
     <h1 class="titulo">Bem-vindo ao Legacy Arena</h1>
+    <div class="row">
+      <!-- Alterado para garantir que todos os cards fiquem na mesma linha -->
+      <?php
+      // Consultas para jogos e torneios
+      $sql = "SELECT * FROM jogos";
+      $conexao = conectar();
+      $resultado = executarSQL($conexao, $sql);
+      $sql2 = "SELECT * FROM torneios";
+      $resultado2 = executarSQL($conexao, $sql2);
 
-    <?php
-
-    $sql = "SELECT * FROM jogos";
-    $conexao = conectar();
-    $resultado = executarSQL($conexao, $sql);
-    $sql2 = "SELECT * FROM torneios";
-    $resultado2 = executarSQL($conexao, $sql2);
-
-    while ($jogo = mysqli_fetch_assoc($resultado)) { ?>
-
-      <div class="card custom-card">
-        <div class="card-image waves-effect waves-block waves-light">
-          <img class="activator" src="imagens/<?= $jogo['imagem'] ?>" alt="Imagem do Card">
+      // Exibindo os jogos na página
+      while ($jogo = mysqli_fetch_assoc($resultado)) { ?>
+        <div class="col s12 m4 l3"> <!-- Aqui podemos ajustar para ocupar mais ou menos espaço em telas maiores -->
+          <div class="card custom-card imgcard">
+            <div class="card-image waves-effect waves-block waves-light">
+              <img class="activator" src="imagens/<?= $jogo['imagem'] ?>" alt="Imagem do Card" height="250px">
+            </div>
+            <div class="card-content">
+              <span class="card-title activator grey-text text-darken-4"> <?= $jogo['nome'] ?> <i class="material-icons right">more_vert</i></span>
+              <p><a href="chaveamentocs.php?id=<?= $jogo['id'] ?>">Acessar Classificação</a></p>
+            </div>
+          </div>
         </div>
-        <div class="card-content">
-          <span class="card-title activator grey-text text-darken-4"> <?= $jogo['nome'] ?> <i class="material-icons right">more_vert</i></span>
-          <p><a href="chaveamentocs.php?id=<?= $jogo['id']  ?>">Acessar Classificação</a></p>
-        </div>
-      </div>
-    <?php  } ?>
-
+      <?php  } ?>
+    </div>
   </div>
-
 </body>
-
-</html>
